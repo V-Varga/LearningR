c20328ff021c81ca89601d99432cfa73c61a559d
Vi Varga <virag.varga.bioinfo@gmail.com>
2023-01-16 15:09:31 +0100

    initial commit

---
+++ R/learning.R
@@ -0,0 +1,23 @@
+# R basics ----------------------------------------------------------------
+
+# printing to the console
+10
+
+# assigning variables
+weight_kilos <- 100
+weight_kilos
+weight_kilos <- 10
+weight_kilos
+
+# using dataframes
+colnames(airquality)
+str(airquality) # str means structure
+summary(airquality)
+
+# styling practice
+2 + 2 # Ctrl+Shift+A
+
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
