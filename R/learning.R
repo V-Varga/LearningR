f60f2b67e8d419afdae14c6e06a99fb6201a0b01
Vi Varga <virag.varga.bioinfo@gmail.com>
2023-01-17 11:29:21 +0100

    learning to use pipes

--- R/learning.R
+++ R/learning.R
@@ -49,6 +49,9 @@ nhanes_small <- select(
 # View the new data frame
 nhanes_small
 
+
+# Fixing variable names ---------------------------------------------------
+
 # Rename all columns to snake case
 nhanes_small <- rename_with(nhanes_small, snakecase::to_snake_case)
 
@@ -70,3 +73,35 @@ nhanes_small %>%
 nhanes_small %>%
   select(phys_active) %>%
   rename(physically_active = phys_active)
+
+
+### Exercise
+
+# 1. Copy and paste the code below into the script file. Replace the ___ in the select() function, with the columns bp_sys_ave, and education.
+
+nhanes_small %>%
+  select(bp_sys_ave, education)
+
+# 2. Copy and paste the code below and fill out the blanks. Rename the bp_ variables so they don’t end in _ave, so they look like bp_sys and bp_dia. Tip: Recall that renaming is in the form new = old.
+
+nhanes_small %>%
+  rename(
+    bp_sys = bp_sys_ave,
+    bp_dia = bp_dia_ave
+  )
+
+# 3. Re-write this piece of code using the “pipe” operator:
+
+select(nhanes_small, bmi, contains("age"))
+
+nhanes_small %>%
+  select(bmi, contains("age"))
+
+# 4. Read through (in your head) the code below. How intuitive is it to read? Now, re-write this code so that you don’t need to create the temporary blood_pressure object by using the pipe, then re-read the revised version. Which do you feel is easier to “read”?
+
+blood_pressure <- select(nhanes_small, starts_with("bp_"))
+rename(blood_pressure, bp_systolic = bp_sys_ave)
+
+nhanes_small %>%
+  select(starts_with("bp_")) %>%
+  rename(bp_systolic = bp_sys_ave)
